637d3113e2799f7c298d0377252b51aff29d80f4
François Gaudin <francois.gaudin@gmail.com>
2017-03-30 18:43:58 +0200

    Enable Swagger2

--- src/main/java/com/fgaudin/namescan/NameScanApplication.java
+++ src/main/java/com/fgaudin/namescan/NameScanApplication.java
@@ -8,13 +8,28 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class NameScanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NameScanApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()                 .apis(RequestHandlerSelectors.basePackage("com.fgaudin.namescan"))
+				.build();
+
 	}
 
 	// TODO create the resource where it is used instead of here
